82944b746283ee05c8f40839bc89d37cd86d69de
charafau <gorudonu@gmail.com>
2018-03-02 12:54:59 +0000

    added picture to app bar

--- lib/widgets/details_screen.dart
+++ lib/widgets/details_screen.dart
@@ -7,77 +7,77 @@ import 'package:recipe/model/recipe.dart';
 import 'package:recipe/widgets/photo.dart';
 import 'package:recipe/widgets/radial_expansion.dart';
 
-class DetailsScreen extends StoreConnector<AppState, AppActions, Recipe> {
+enum AppBarBehavior { normal, pinned, floating, snapping }
 
+class DetailsScreen extends StoreConnector<AppState, AppActions, Recipe> {
   static const double kMinRadius = 32.0;
   static const double kMaxRadius = 128.0;
   static const opacityCurve =
-  const Interval(0.0, 0.75, curve: Curves.fastOutSlowIn);
+      const Interval(0.0, 0.75, curve: Curves.fastOutSlowIn);
 
   final int recipePosition;
 
-  DetailsScreen({ @required this.recipePosition});
+  DetailsScreen({@required this.recipePosition});
 
   static RectTween _createRectTween(Rect begin, Rect end) {
     return new MaterialRectCenterArcTween(begin: begin, end: end);
   }
 
-//  @override
-//  Widget build(BuildContext context, AppState state, AppActions actions) {
-//    return new Scaffold(
-//      appBar: new AppBar(
-//        title: new Text('Details'),
-//      ),
-//      body: new Center(
-//        child: new SizedBox(
-//          width: kMaxRadius * 2.0,
-//          height: kMaxRadius * 2.0,
-//          child: new Hero(
-//            createRectTween: _createRectTween,
-//            tag: recipe.title,
-//            child: new RadialExpansion(
-//              maxRadius: kMaxRadius,
-//              child: new Photo(
-//                photo: recipe.thumbnail,
-//                onTap: () {
-//                  Navigator.of(context).pop();
-//                },
-//              ),
-//            ),
-//          ),
-//        ),
-//      ),
-//    );
-//  }
-
   @override
   Recipe connect(AppState state) => state.result.results[recipePosition];
+
+  final double _appBarHeight = 256.0;
+  AppBarBehavior _appBarBehavior = AppBarBehavior.pinned;
 
   @override
   Widget build(BuildContext context, Recipe state, AppActions actions) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Details'),
-      ),
-      body: new Center(
-        child: new SizedBox(
-          width: kMaxRadius * 2.0,
-          height: kMaxRadius * 2.0,
-          child: new Hero(
-            createRectTween: _createRectTween,
-            tag: state.title,
-            child: new RadialExpansion(
-              maxRadius: kMaxRadius,
-              child: new Photo(
-                photo: state.thumbnail,
-                onTap: () {
-                  Navigator.of(context).pop();
-                },
-              ),
+        body: new CustomScrollView(
+      slivers: <Widget>[
+        new SliverAppBar(
+          expandedHeight: _appBarHeight,
+          pinned: _appBarBehavior == AppBarBehavior.pinned,
+          floating: _appBarBehavior == AppBarBehavior.floating ||
+              _appBarBehavior == AppBarBehavior.snapping,
+          snap: _appBarBehavior == AppBarBehavior.snapping,
+          flexibleSpace: new FlexibleSpaceBar(
+            title: new Text(state.title),
+            background: new Stack(
+              fit: StackFit.expand,
+              children: <Widget>[
+                new Hero(
+                  createRectTween: _createRectTween,
+                  tag: state.title,
+                  child: new Image.network(
+                    state.thumbnail,
+                    fit: BoxFit.cover,
+                    height: _appBarHeight,
+                  ),
+                ),
+              ],
             ),
           ),
         ),
-      ),
-    );
+        new SliverList(
+            delegate: new SliverChildListDelegate(<Widget>[
+          new Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: new Text('Igredients',
+                style:
+                    new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)),
+          ),
+          new Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: _buildIngredients(state.ingredients),
+          ),
+        ])),
+      ],
+    ));
+  }
+
+  Widget _buildIngredients(String ingredients) {
+    List<String> split = ingredients.replaceAll(' ', '').split(',');
+    final reduce = split.reduce((value, element) => value + "\n" + element);
+    return new Text('' + reduce);
   }
 }
